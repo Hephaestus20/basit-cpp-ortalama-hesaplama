22d623effb644a605a88ee611ebc59e8c245dda4
Hephaestus20 <85035055+Hephaestus20@users.noreply.github.com>
2021-10-19 20:23:46 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,45 @@
+﻿// -_- Algoritma, Sözel Kod -_-
+
+// 1) Başla
+// 2) Birinci sözlü notu alınız (birincinot,b)
+// 3) İkinci sözlü notu alınız (ikincinot,i)
+// 4) Performans notu alınız (performans,p)
+// 5) tüm notları topla (b+i+p = topla)
+// 6) toplanan sayıları üçe böl (topla / 3 = ortalama)
+// 7) ortalama eğer 60dan büyük ise geçti değil ise kaldı yazısı yansıt. (ortalama 60 > veya <)
+// 8) Bitir.
+
+
+
+#include<iostream>
+
+using namespace std;
+
+
+int main() {
+	int birincinot, ikincinot, performans, toplam, ortalama;
+
+	cout << "Lutfen birinci notu giriniz: ";
+	cin >> birincinot;
+
+	cout << "Lutfen ikinci notu giriniz: ";
+	cin >> ikincinot;
+
+	cout << "Lutfen performans notu giriniz: ";
+	cin >> performans;
+
+	toplam = birincinot + ikincinot + performans;
+	ortalama = toplam / 3;
+
+
+	if (toplam > 60) {
+		cout << "Ortalamaniz: " << ortalama << endl;
+		cout << "Tebrikler sinifi gectiniz.";
+	}
+	else {
+		cout << "Ortalamaniz: " << ortalama << endl;
+		cout << "Sinifta Kaldiniz...";
+	}
+
+	return 0;
+}
